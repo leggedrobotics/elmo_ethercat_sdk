47ad71a1ba3ccb1b8e1b402c9dffca174426d5ef
Jonas Junger <jungerj.engineer@gmail.com>
2020-11-13 15:11:03 +0100

    Added missing licenses

--- src/elmo_ethercat_sdk/Elmo.cpp
+++ src/elmo_ethercat_sdk/Elmo.cpp
@@ -1,3 +1,23 @@
+/*
+** Copyright (2019-2020) Robotics Systems Lab - ETH Zurich:
+** Jonas Junger, Johannes Pankert, Fabio Dubois, Lennart Nachtigall,
+** Markus Staeuble
+**
+** This file is part of the elmo_ethercat_sdk.
+** The elmo_ethercat_sdk is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** The elmo_ethercat_sdk is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with the elmo_ethercat_sdk. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "elmo_ethercat_sdk/Elmo.hpp"
 #include "elmo_ethercat_sdk/ConfigurationParser.hpp"
 #include "elmo_ethercat_sdk/ObjectDictionary.hpp"
